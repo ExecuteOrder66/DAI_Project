155062505c6ef1988144dd9b7cb792e1548bef7b
ExecuteOrder66 <rcferradas@esei.uvigo.es>
2019-10-07 12:10:23 +0200

    Revert "Control Excepciones + notificacion a cliente"
    This reverts commit df19dbe8324f5a78fc8e2be5542bb560ddcfcb61.

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/ServiceThread.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/ServiceThread.java
@@ -13,82 +13,74 @@ import es.uvigo.esei.dai.hybridserver.http.HTTPRequestMethod;
 import es.uvigo.esei.dai.hybridserver.http.HTTPResponse;
 import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
 
-public class ServiceThread implements Runnable {
+public class ServiceThread implements Runnable{
 
 	private Socket clientSocket;
 	private Controller controller;
-
+	
 	public ServiceThread(Socket client, Controller control) {
 		this.clientSocket = client;
 		this.controller = control;
 	}
-
+	
 	@Override
 	public void run() {
-		try (Socket socket = this.clientSocket) {
+		try(Socket socket = this.clientSocket){
 			// http://localhost:8888/html
 			// http://localhost:8888/html?uuid=6df1047e-cf19-4a83-8cf3-38f5e53f7725
-			try {	//try para capturar excepciones y poder notificar al cliente de los errores
-				Reader reader = new InputStreamReader(socket.getInputStream());
-				HTTPRequest request = new HTTPRequest(reader);
-				Writer writer = new OutputStreamWriter(socket.getOutputStream());
-				HTTPResponse response = new HTTPResponse();
+			Reader reader = new InputStreamReader(socket.getInputStream());
+			HTTPRequest request = new HTTPRequest(reader);
+			Writer writer = new OutputStreamWriter(socket.getOutputStream());
+			HTTPResponse response = new HTTPResponse();
 
-				if (request.getResourceName().equals("html")) { // comprobar error 400
-					if (request.getMethod().equals(HTTPRequestMethod.GET)) {
-						if (request.getResourceParameters().isEmpty()) {
-							response.setContent(controller.getIndex());
-						} else {
-							String uuid = request.getResourceParameters().get("uuid");
-							if (controller.isPage(uuid)) { // comprobar error 404
-								response.setContent(controller.getPage(uuid));
-							} else {
-								String error404 = "<html><head></head><body>ERROR 404: page not found</body></html>";
-								response.setStatus(HTTPResponseStatus.forCode(404));
-								response.setContent(error404);
-							}
-						}
-
+			
+			if(request.getResourceName().equals("html")) { //comprobar error 400
+				if(request.getMethod().equals(HTTPRequestMethod.GET)) {
+					if(request.getResourceParameters().isEmpty()) {
+						response.setContent(controller.getIndex());	
 					} else {
-						if (request.getMethod().equals(HTTPRequestMethod.POST)) {
-							controller.addPage(request.getContent());
-						}
-						if (request.getMethod().equals(HTTPRequestMethod.DELETE)) {
-							String uuid = request.getResourceParameters().get("uuid");
-							if (controller.deletePage(uuid) != null) {
-								response.setContent(
-										"<html><head></head><body>Borrada la pagina: " + uuid + "</body></html>");
-							} else {
-								String error404 = "<html><head></head><body>ERROR 404: page not found</body></html>";
-								response.setStatus(HTTPResponseStatus.forCode(404));
-								response.setContent(error404);
-							}
+						String uuid = request.getResourceParameters().get("uuid");
+						if(controller.isPage(uuid)) {	//comprobar error 404
+							response.setContent(controller.getPage(uuid));
+						} else {
+							String error404 = "<html><head></head><body>ERROR 404: page not found</body></html>";
+							response.setStatus(HTTPResponseStatus.forCode(404));
+							response.setContent(error404);
+						}		
+					}
+
+				} else {
+					if(request.getMethod().equals(HTTPRequestMethod.POST)) {
+						controller.addPage(request.getContent());	
+					}
+					if(request.getMethod().equals(HTTPRequestMethod.DELETE)) {
+						String uuid = request.getResourceParameters().get("uuid");
+						if(controller.deletePage(uuid) != null) {
+							response.setContent( "<html><head></head><body>Borrada la pagina: " + uuid +"</body></html>");
+						}else {
+							String error404 = "<html><head></head><body>ERROR 404: page not found</body></html>";
+							response.setStatus(HTTPResponseStatus.forCode(404));
+							response.setContent(error404);
 						}
 					}
-					response.print(writer); // Enviamos respuesta
-				} else {
-					String error400 = "<html><head></head><body>ERROR 400: bad request</body></html>";
-					response.setStatus(HTTPResponseStatus.forCode(400));
-					response.setContent(error400);
-					response.print(writer);
 				}
-			} catch (HTTPParseException eHTTP) {
-				// ERROR PARSEO HTTP
-				//?
-				HTTPResponse response = new HTTPResponse();
-				String error400 = "<html><head></head><body>ERROR 400: bad request ParseException</body></html>";
+			response.print(writer);		//Enviamos respuesta
+			} else {
+				String error400 = "<html><head></head><body>ERROR 400: bad request</body></html>";
 				response.setStatus(HTTPResponseStatus.forCode(400));
 				response.setContent(error400);
-				eHTTP.printStackTrace();
-			} catch (IOException eIO) {
-				// ERROR 500
-				HTTPResponse response = new HTTPResponse();
-				String error500 = "<html><head></head><body>ERROR 500: Internal Server Error</body></html>";
-				response.setStatus(HTTPResponseStatus.forCode(500));
-				response.setContent(error500);
-				eIO.printStackTrace();
+				response.print(writer);
 			}
-		} catch (Exception e) {
+			
+		}catch(HTTPParseException eHTTP) {
+			//ERROR PARSEO HTTP
+			eHTTP.printStackTrace();
+		}catch(IOException e) {
+			//Duda si capturar aqui la IOException o si capturarla en el serverThread 
+			//o si capturarla y relanzarla
+			//ERROR 500
+			String error500 = "<html><head></head><body>ERROR 500: Internal Server Error</body></html>";
+			
 			e.printStackTrace();
 		}
 	}
