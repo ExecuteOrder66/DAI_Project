c39c110c06c48f61a5219df5295658356d9a2fec
abmartinez-cmd <55605916+abmartinez-cmd@users.noreply.github.com>
2020-01-10 19:15:45 +0100

    Add files via upload

---
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/controller/WSController.java
@@ -0,0 +1,63 @@
+package es.uvigo.esei.dai.hybridserver.controller;
+
+import java.util.List;
+
+import es.uvigo.esei.dai.hybridserver.ws.HybridServerService;
+
+public class WSController implements HybridServerService{
+
+	@Override
+	public List<String> getHTMLuuids() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getXMLuuids() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getXSDuuids() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getXSLTuuids() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHTMLContent(String htmlId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getXMLContent(String xmlId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getXSDContent(String xsdId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getXSLTContent(String xsltId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getXSDuuid(String xsltId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
