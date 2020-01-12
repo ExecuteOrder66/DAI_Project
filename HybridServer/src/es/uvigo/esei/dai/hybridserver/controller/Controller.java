f8f972aa774f218c8327797393a37bc13fbec1b3
abmartinez-cmd <55605916+abmartinez-cmd@users.noreply.github.com>
2020-01-12 13:08:04 +0100

    Add files via upload

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/controller/Controller.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/controller/Controller.java
@@ -1,16 +1,27 @@
 package es.uvigo.esei.dai.hybridserver.controller;
 
+import java.net.MalformedURLException;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 import es.uvigo.esei.dai.hybridserver.InvalidPageException;
+import es.uvigo.esei.dai.hybridserver.ServerConfiguration;
 import es.uvigo.esei.dai.hybridserver.model.dao.DAO;
+import es.uvigo.esei.dai.hybridserver.ws.HybridServerService;
+import es.uvigo.esei.dai.hybridserver.ws.WebServiceConnection;
 
 public class Controller implements DAO{
 	private final DAO dao;
+	private List<ServerConfiguration> serverConfList = new LinkedList<ServerConfiguration>();
 	
 	public Controller(DAO dao) {
 		this.dao = dao;
+	}
+	
+	public Controller(DAO dao, List<ServerConfiguration> serverConfList) {
+		this.dao = dao;
+		this.serverConfList = serverConfList;
 	}
 	
 	@Override
@@ -25,7 +36,23 @@ public class Controller implements DAO{
 	
 	@Override
 	public String getPage(String uuid, String contentType) throws InvalidPageException {
-		return this.dao.getPage(uuid, contentType);
+		String content = this.dao.getPage(uuid, contentType);
+		if(content == null) {
+			try {
+				WebServiceConnection ws = new WebServiceConnection(this.serverConfList);
+				List<HybridServerService> hybridServerServiceList = ws.getServers();
+				for(HybridServerService server : hybridServerServiceList) {
+					content = server.getContent(uuid, contentType);
+					if(content != null) {
+						return content;
+					}
+				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} 
+		return content;
 	}
 	
 	@Override
@@ -36,6 +63,10 @@ public class Controller implements DAO{
 	@Override
 	public String deletePage(String uuid, String contentType) throws SQLException {
 		return this.dao.deletePage(uuid, contentType);
+	}
+	
+	public String getXSDContent(String xsltId) throws InvalidPageException {
+		return this.dao.getXSDContent(xsltId);
 	}
 		
 }
